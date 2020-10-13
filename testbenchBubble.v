fc8c9ceee9ab88083add82c8172058c286769263
movingpictures83 <movingpictures83@gmail.com>
2020-10-13 12:14:45 -0400

    Lengthened clock period to be more realistic

--- testbenchBubble.v
+++ testbenchBubble.v
@@ -1,10 +1,9 @@
 `timescale 1ps / 1ps
 module testbenchBubble();
-// Clock should be set to period=100 to accomodate memory
-// Total execution time: 85900ps
+// Approximating clock period as 100+100 (two accesses to RAM) + 100 (everything else)
    reg clk_tb, rst_tb;
    
-   localparam CLK_PERIOD = 100;
+   localparam CLK_PERIOD = 300;
    
    CPU_SingleCycle myCPU(clk_tb, rst_tb);
    
@@ -19,23 +18,24 @@ module testbenchBubble();
       myCPU.b2v_im.memory[4] = 'b00100010001100101111111111111111; // addi $s2, $s1, -1
       myCPU.b2v_im.memory[5] = 'b00100000000011010000000000000001; // addi $t5, $zero, 1
       myCPU.b2v_im.memory[6] = 'b00000000000100100100000000101010; // loop1:   slt $t0, $zero, $s2 
-      myCPU.b2v_im.memory[7] = 'b00010001000000000000000000010000; // beq $t0, $zero, doneloop1
+      myCPU.b2v_im.memory[7] = 'b00010001000000000000000000010001; // beq $t0, $zero, doneloop1
       myCPU.b2v_im.memory[8] = 'b00100000000100110000000000000000; // addi $s3, $zero, 0
       myCPU.b2v_im.memory[9] = 'b00000010011100100100100000101010; // loop2:   slt $t1, $s3, $s2
-      myCPU.b2v_im.memory[10] = 'b00010001001000000000000000001011; // beq $t1, $zero, doneloop2
+      myCPU.b2v_im.memory[10] = 'b00010001001000000000000000001100; // beq $t1, $zero, doneloop2
       myCPU.b2v_im.memory[11] = 'b00000010011100110101000000100000; // add $t2, $s3, $s3
       myCPU.b2v_im.memory[12] = 'b00000001010010100101000000100000; // add $t2, $t2, $t2
       myCPU.b2v_im.memory[13] = 'b00000010000010100101100000100000; // add $t3, $s0, $t2
       myCPU.b2v_im.memory[14] = 'b10001101011101000000000000000000; // lw $s4, 0($t3)
       myCPU.b2v_im.memory[15] = 'b10001101011101010000000000000100; // lw $s5, 4($t3)
       myCPU.b2v_im.memory[16] = 'b00000010101101000110000000101010; // slt $t4, $s5, $s4 
-      myCPU.b2v_im.memory[17] = 'b00010001100000000000000000000010; // beq $t4, $zero, doneif
+      myCPU.b2v_im.memory[17] = 'b00010001100000000000000000000011; // beq $t4, $zero, doneif
       myCPU.b2v_im.memory[18] = 'b10101101011101010000000000000000; // sw $s5, 0($t3)
-      myCPU.b2v_im.memory[19] = 'b10101101011101000000000000000100; // sw $s4, 4($t3)
-      myCPU.b2v_im.memory[20] = 'b00100010011100110000000000000001; // doneif:    addi $s3, $s3, 1
-      myCPU.b2v_im.memory[21] = 'b00001000000000000000000000001001; // j loop2 //instruction 9
-      myCPU.b2v_im.memory[22] = 'b00100010010100101111111111111111; //doneloop2: addi $s2, $s2, -1
-      myCPU.b2v_im.memory[23] = 'b00001000000000000000000000000110; // j loop1 //instruction 6
+      myCPU.b2v_im.memory[19] = 'b00100001011010110000000000000100; // addi $t3, $t3, 4
+      myCPU.b2v_im.memory[20] = 'b10101101011101000000000000000000; // sw $s4, 0($t3)
+      myCPU.b2v_im.memory[21] = 'b00100010011100110000000000000001; // doneif:    addi $s3, $s3, 1
+      myCPU.b2v_im.memory[22] = 'b00001000000000000000000000001001; // j loop2 //instruction 9
+      myCPU.b2v_im.memory[23] = 'b00100010010100101111111111111111; //doneloop2: addi $s2, $s2, -1
+      myCPU.b2v_im.memory[24] = 'b00001000000000000000000000000110; // j loop1 //instruction 6
       // doneloop1:
       // Next instruction, uses myCPU.b2v_im.memory[4]
 
@@ -73,7 +73,7 @@ module testbenchBubble();
         //////////////////////////////////////////////////
         // CHANGE PC VALUE IN THIS IF STATEMENT
         // ADD 4 TIMES THE AMOUNT OF INSTRUCTIONS YOU RUN
-        if(myCPU.b2v_PC.Q === 96) begin
+        if(myCPU.b2v_PC.Q === 100) begin
         //////////////////////////////////////////////////
         // CHANGE THIS TEST
         // CURRENT TEST ASSUMES YOU SWAPPED THE THIRD AND SIXTH

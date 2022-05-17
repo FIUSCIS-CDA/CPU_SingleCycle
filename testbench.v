d98c625199e7d96971f6f1b30d44f1198273ac21
movingpictures83 <movingpictures83@gmail.com>
2022-05-17 15:26:37 -0400

    Fixed comments

--- testbench.v
+++ testbench.v
@@ -1,61 +1,74 @@
-//`timescale 1ps / 1ps
+///////////////////////////////////////////////////////////////////////////////////
+// Testbench for Component: CPU_SingleCycle (CLK=200)
+// Package: FIUSCIS-CDA
+// Course: CDA3102 (Computer Architecture), Florida International University
+// Developer: Trevor Cickovski
+// Extended By: CDA3102 students
+// License: MIT, (C) 2020-2022 All Rights Reserved
+///////////////////////////////////////////////////////////////////////////////////
+
 module testbench();
-
-
-// Approximating clock period as 100+100 (two accesses to RAM)
+`include "../Test/Test.v"
+///////////////////////////////////////////////////////////////////////////////////
+// Inputs: clk, reset (1-bit)
    reg clk, rst;
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+// Outputs: Overflow (1-bit), PC (32-bit), OPCODE (6-bit) (For WAVE)
    wire Overflow;
    wire[31:0] PC;
    wire[31:26] OPCODE;
+///////////////////////////////////////////////////////////////////////////////////
+ 
+   integer address;
+
+///////////////////////////////////////////////////////////////////////////////////
+// Component is CLOCKED
+// Set clk period to 200 in wave
+// Approximating clock period as 100+100 (two accesses to RAM)
+localparam CLK_PERIOD=200;
+///////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CPU will check if the array sorted if PC hits this value
+// Set to location of doneloop1 below
+localparam TERMINALPC=92;
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
-
-   localparam CLK_PERIOD = 200;
-   
-   CPU_SingleCycle myCPU(clk, rst, Overflow, OPCODE, PC);
+   CPU_SingleCycle myCPU(.clk(clk), .reset(rst), .Overflow(Overflow), .OPCODE(OPCODE), .PC(PC));
    
    initial begin
-      // initialize instruction memory
-      myCPU.b2v_im.memory[0] = 'b00100000000100000000001000000000; // addi $s0, $zero, 512   # Assume arr=512
-      myCPU.b2v_im.memory[1] = 'b00100000000100010000000000001100; // addi $s1, $zero, 12    # Size (N) is 12  
-      myCPU.b2v_im.memory[2] = 'b00100000000100100000000000000010; // addi $s2, $zero, 2     # i = 2
-      myCPU.b2v_im.memory[3] = 'b00100000000100110000000000000101; // addi $s3, $zero, 5     # j = 5
-      /////////////////////////////////////////////////////////////
-      // PUT BUBBLE SORT CODE HERE
-      myCPU.b2v_im.memory[4] = 'b00100010001100101111111111111111; // addi $s2, $s1, -1
-      myCPU.b2v_im.memory[5] = 'b00100000000011010000000000000001; // addi $t5, $zero, 1
-      myCPU.b2v_im.memory[6] = 'b00000000000100100100000000101010; // loop1:   slt $t0, $zero, $s2 
-      //myCPU.b2v_im.memory[7] = 'b00010001000000000000000000010001; // beq $t0, $zero, doneloop1
-      myCPU.b2v_im.memory[7] = 'b00010101000011010000000000010001; // bne $t0, $t5, doneloop1
-      myCPU.b2v_im.memory[8] = 'b00100000000100110000000000000000; // addi $s3, $zero, 0
-      myCPU.b2v_im.memory[9] = 'b00000010011100100100100000101010; // loop2:   slt $t1, $s3, $s2
-      myCPU.b2v_im.memory[10] = 'b00010001001000000000000000001100; // beq $t1, $zero, doneloop2
-      //myCPU.b2v_im.memory[10] = 'b00010101001011010000000000001100; // bne $t1, $t5, doneloop2
-      myCPU.b2v_im.memory[11] = 'b00000010011100110101000000100000; // add $t2, $s3, $s3
-      myCPU.b2v_im.memory[12] = 'b00000001010010100101000000100000; // add $t2, $t2, $t2
-      myCPU.b2v_im.memory[13] = 'b00000010000010100101100000100000; // add $t3, $s0, $t2
-      myCPU.b2v_im.memory[14] = 'b10001101011101000000000000000000; // lw $s4, 0($t3)
-      myCPU.b2v_im.memory[15] = 'b10001101011101010000000000000100; // lw $s5, 4($t3)
-      myCPU.b2v_im.memory[16] = 'b00000010101101000110000000101010; // slt $t4, $s5, $s4 
-      myCPU.b2v_im.memory[17] = 'b00010001100000000000000000000011; // beq $t4, $zero, doneif
-      //myCPU.b2v_im.memory[17] = 'b00010101100011010000000000000011; // bne $t4, $t5, doneif
-      myCPU.b2v_im.memory[18] = 'b10101101011101010000000000000000; // sw $s5, 0($t3)
-      myCPU.b2v_im.memory[19] = 'b00100001011010110000000000000100; // addi $t3, $t3, 4
-      myCPU.b2v_im.memory[20] = 'b10101101011101000000000000000000; // sw $s4, 0($t3)
-      myCPU.b2v_im.memory[21] = 'b00100010011100110000000000000001; // doneif:    addi $s3, $s3, 1
-      myCPU.b2v_im.memory[22] = 'b00001000000000000000000000001001; // j loop2 //instruction 9
-      myCPU.b2v_im.memory[23] = 'b00100010010100101111111111111111; //doneloop2: addi $s2, $s2, -1
-      myCPU.b2v_im.memory[24] = 'b00001000000000000000000000000110; // j loop1 //instruction 6
-     myCPU.b2v_im.memory[25] = 'b10001100000010000000001000110000; // lw $t0, 560($zero)
-      myCPU.b2v_im.memory[26] = 'b00100001000010000000000000000001; // addi $t0, $t0, 1
-      // doneloop1:
-      // Next instruction, uses myCPU.b2v_im.memory[4]
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // Initialize Instruction Memory with MIPS Bubble Sort       //          INSTRUCTION                      PC
+      myCPU.b2v_im.memory[0] = 'b00100000000100000000001000000000; //          addi $s0, $zero, 512  		0	
+      myCPU.b2v_im.memory[1] = 'b00100000000100010000000000001011; //          addi $s1, $zero, 11		4	
+      myCPU.b2v_im.memory[2] = 'b00000000000000001001000000100000; //          add $s2, $zero, zero		8
+      myCPU.b2v_im.memory[3] = 'b00100000000011010000000000000001; //          addi $t5, $zero, 1		12
+      myCPU.b2v_im.memory[4] = 'b00101010010010000000000000001011; // loop1:   slti $t0, $s2, 11          	16   
+      myCPU.b2v_im.memory[5] = 'b00010101000011010000000000010001; // 	       bne $t0, $t5, doneloop1          20            
+      myCPU.b2v_im.memory[6] = 'b00000000000000001001100000100000; // 	       add $s3, $zero, $zero		24  
+      myCPU.b2v_im.memory[7] = 'b00000010001100100111000000100010; //          sub $t6, $s1, $s2		28         
+      myCPU.b2v_im.memory[8] = 'b00000010011011100100100000101010; // loop2:   slt $t1, $s3, $t6		32
+      myCPU.b2v_im.memory[9] = 'b00010001001000000000000000001011; // 	       beq $t1, $zero, doneloop2        36        
+      myCPU.b2v_im.memory[10] = 'b00000010011100110101000000100000; // 	       add $t2, $s3, $s3                40       
+      myCPU.b2v_im.memory[11] = 'b00000001010010100101000000100000; // 	       add $t2, $t2, $t2                44       
+      myCPU.b2v_im.memory[12] = 'b00000010000010100101100000100000; // 	       add $t3, $s0, $t2                48       
+      myCPU.b2v_im.memory[13] = 'b10001101011101000000000000000000; // 	       lw $s4, 0($t3)                   52       
+      myCPU.b2v_im.memory[14] = 'b10001101011101010000000000000100; // 	       lw $s5, 4($t3)                   56       
+      myCPU.b2v_im.memory[15] = 'b00000010101101000110000000101010; // 	       slt $t4, $s5, $s4                60       
+      myCPU.b2v_im.memory[16] = 'b00010001100000000000000000000010; // 	       beq $t4, $zero, doneif           64        
+      myCPU.b2v_im.memory[17] = 'b10101101011101010000000000000000; // 	       sw $s5, 0($t3)                   68       
+      myCPU.b2v_im.memory[18] = 'b10101101011101000000000000000100; // 	       sw $s4, 4($t3)                   72       
+      myCPU.b2v_im.memory[19] = 'b00100010011100110000000000000001; // doneif: addi $s3, $s2, 1                 76	
+      myCPU.b2v_im.memory[20] = 'b00001000000000000000000000000111; // 	       j loop2  			80
+      myCPU.b2v_im.memory[21] = 'b00100010010100100000000000000001; //doneloop2:addi $s2, $s2, 1		84
+      myCPU.b2v_im.memory[22] = 'b00001000000000000000000000000011; // 	       j loop1 (3)			88
+      							            // doneloop1:                               92
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-      /////////////////////////////////////////////////////////////
-
-
-      // Initialized array manually
+      /////////////////////////////////////////////////////////////////////////////////////////////
+      // Initial Array (Unsorted)
       myCPU.b2v_DM.memory[512 >> 2] = 55;
       myCPU.b2v_DM.memory[516 >> 2] = 88;
       myCPU.b2v_DM.memory[520 >> 2] = 0;
@@ -68,63 +81,32 @@ module testbench();
       myCPU.b2v_DM.memory[548 >> 2] = 66;
       myCPU.b2v_DM.memory[552 >> 2] = 121;
       myCPU.b2v_DM.memory[556 >> 2] = 44;
-      myCPU.b2v_DM.memory[560 >> 2] = 2**31 - 1;  // Big number
+      /////////////////////////////////////////////////////////////////////////////////////////////
+ 
+      /////////////////////////////////////////////////////////////////////////////////////////////
+      // Turn power on for 1/2 tick
       rst <= 1;  # (CLK_PERIOD/2);
+      /////////////////////////////////////////////////////////////////////////////////////////////
       rst <= 0; 
-   end
-
-
-   // Generate clock
-   always @*
-   begin
-      clk <= 1;       # (CLK_PERIOD/2);
-      clk <= 0;       # (CLK_PERIOD/2);
-   end
-   
+   end 
 
   always@(negedge clk)
 
     begin
-
-
-        //////////////////////////////////////////////////
-        // CHANGE PC VALUE IN THIS IF STATEMENT
-        // ADD 4 TIMES THE AMOUNT OF INSTRUCTIONS YOU RUN
-        if(PC === 104) begin
-	if (Overflow == 0) begin
-	     $display("Error: Expected overflow when adding 2**31+1");
-             //$stop;
-        end
-        //////////////////////////////////////////////////
-        // CHANGE THIS TEST
-        // CURRENT TEST ASSUMES YOU SWAPPED THE THIRD AND SIXTH
-          if ( 
-             (myCPU.b2v_DM.memory[512 >> 2] < myCPU.b2v_DM.memory[516 >> 2]) &&
-             (myCPU.b2v_DM.memory[516 >> 2] < myCPU.b2v_DM.memory[520 >> 2]) &&
-             (myCPU.b2v_DM.memory[520 >> 2] < myCPU.b2v_DM.memory[524 >> 2]) &&
-             (myCPU.b2v_DM.memory[524 >> 2] < myCPU.b2v_DM.memory[528 >> 2]) &&
-             (myCPU.b2v_DM.memory[528 >> 2] < myCPU.b2v_DM.memory[532 >> 2]) &&
-             (myCPU.b2v_DM.memory[532 >> 2] < myCPU.b2v_DM.memory[536 >> 2]) &&
-             (myCPU.b2v_DM.memory[536 >> 2] < myCPU.b2v_DM.memory[540 >> 2]) &&
-             (myCPU.b2v_DM.memory[540 >> 2] < myCPU.b2v_DM.memory[544 >> 2]) &&
-             (myCPU.b2v_DM.memory[544 >> 2] < myCPU.b2v_DM.memory[548 >> 2]) &&
-             (myCPU.b2v_DM.memory[548 >> 2] < myCPU.b2v_DM.memory[552 >> 2]) &&
-             (myCPU.b2v_DM.memory[552 >> 2] < myCPU.b2v_DM.memory[556 >> 2])  )
-
-
- begin
-             $display("CPU functional");
-             $stop;
+        ///////////////////////////////////////////////////////////////////////////////////
+        // When we hit the terminal PC, verify every pair of elements is ascending
+        // This implies the array is sorted, and bubble sort worked correctly
+        if(PC === TERMINALPC) begin
+          for (address=512; address <= 552; address = address + 4)
+          begin
+             $display("Testing element %d and element %d", address, address+4);
+             verifyLessEqual32(myCPU.b2v_DM.memory[address >> 4],
+                               myCPU.b2v_DM.memory[(address+4) >> 4]);
           end
-          else begin
-             $display("CPU not functional");
-             $stop;
-           end
-       end
-	else if (Overflow == 1) begin
-	     $display("Error: Did not expect overflow during the regular MIPS program");
-             $stop;
-        end
+          $display("CPU functional");
+          $stop;
+         end
+        ///////////////////////////////////////////////////////////////////////////////////
    end
  
 endmodule
